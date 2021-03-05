9af4079a4e7ddd82c3e0c17113cdc22aa2faeb3b
天宇 <353896627@qq.com>
2021-03-05 15:41:18 +0800

    feat: 初始化项目

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  title: 'Yasimola',
+  description: '',
+  themeConfig: {
+    sidebar: [
+      {
+        title: '工具',
+        children: [
+          {
+            title: 'npm包大小查询',
+            path: 'http://wangzaixst.xyz/npm'
+          }
+        ]
+      },
+      {
+        title: 'VIP影视解析',
+        path: '/video/'
+      },
+      {
+        title: 'NPS内网穿透',
+        path: 'http://tiany.wangzaixst.xyz/'
+      },
+      {
+        title: '挖矿教程',
+        path: '/eth/'
+      }
+    ]
+  }
+}
---
+++ docs/video/README.md
@@ -0,0 +1,14 @@
+# VIP视频解析地址
+
+使用方法<br>
+https://example.com/?url=(VIP视频网页地址)
+
+[播放地址](http://wangzaixst.xyz/video)
+
+[解析地址1](https://app.paintstrain.com/ybjx/?url=)<br>
+[解析地址2](https://api.lytwx.top/yiba/?url=)<br>
+[解析地址3](http://kj.gouhys.cn/appzy/08041643/?url=)<br>
+[解析地址4](http://qy.wuubaa.com/appzy/09262199/?url=)<br>
+[解析地址5](https://www.8090g.cn/jiexi/?url=)
+
+app开发中....
